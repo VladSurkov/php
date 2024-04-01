90269e5df3fa680dd51080e7149351875d338f8d
VladSurkov <surkov.vladyslav.34@gmail.com>
2024-04-02 00:19:21 +0300

    added first part 2 lab

---
+++ public/lab2/main.php
@@ -0,0 +1,6 @@
+<?php
+  echo "<div style='margin-bottom: 10px;'><a href='1.php'>First task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='2.php'>Second task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='3.php'>Third task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='4.php'>Fourth task</a></div>";
+?>
