a48737bf249e61fd1948ab6a7b6404677becce3f
VladSurkov <surkov.vladyslav.34@gmail.com>
2024-04-03 00:53:10 +0300

    finished second lab

--- public/lab2/main.php
+++ public/lab2/main.php
@@ -3,4 +3,14 @@
   echo "<div style='margin-bottom: 10px;'><a href='2.php'>Second task</a></div>";
   echo "<div style='margin-bottom: 10px;'><a href='3.php'>Third task</a></div>";
   echo "<div style='margin-bottom: 10px;'><a href='4.php'>Fourth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='5.php'>Fifth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='6.php'>Sixth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='7.php'>Seventh task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='8.php'>Eighth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='9.php'>Ninth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='10.php'>Tenth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='11.php'>Eleventh task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='12.php'>Twelfth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='13.php'>Thirteenth task</a></div>";
+  echo "<div style='margin-bottom: 10px;'><a href='14.php'>Fourteenth task</a></div>";
 ?>
